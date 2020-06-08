b49f92a57627514eb981ac304f8fa2e21eae5c90
Tobias Steinel <steinel@neundreiviertel.io>
2020-06-08 21:40:10 +0200

    ci: 🎡 prepared for usage as node module
    Took 1 minute

---
+++ index.ts
@@ -0,0 +1,34 @@
+/* Entities */
+import { AddressEntity } from './address/address.entity';
+import { CategoryEntity } from './category/category.entity';
+import { InstitutionEntity } from './institution/institution.entity';
+import { InstitutionAdminEntity } from './institutionadmin/institutionAdmin.entity';
+import { InstitutionalItemEntity } from './institutionalItem/institutionalItem.entity';
+import { InstitutionalOfferEntity } from './institutionalOffer/institutionalOffer.entity';
+import { InstitutionalRequestEntity } from './institutionalRequest/institutionalRequest.entity';
+import { InstitutionMemberEntity } from './institutionMember/institutionMember.entity';
+import { InstitutionTypeEntity } from './institutionType/institutionType.entity';
+import { PrivateItemEntity } from './privateItem/institutionalItem.entity';
+import { PrivateOfferEntity } from './privateOffer/privateOffer.entity';
+import { PrivateRequestEntity } from './privateRequest/privateRequest.entity';
+import { RewardCatalogueEntity } from './rewardCatalogue/rewardCatalogue.entity';
+import { TagEntity } from './tag/tag.entity';
+import { UserEntity } from './user/user.entity';
+
+export = {
+  AddressEntity,
+  CategoryEntity,
+  InstitutionEntity,
+  InstitutionAdminEntity,
+  InstitutionalItemEntity,
+  InstitutionalOfferEntity,
+  InstitutionalRequestEntity,
+  InstitutionMemberEntity,
+  InstitutionTypeEntity,
+  PrivateItemEntity,
+  PrivateOfferEntity,
+  PrivateRequestEntity,
+  RewardCatalogueEntity,
+  TagEntity,
+  UserEntity
+};
