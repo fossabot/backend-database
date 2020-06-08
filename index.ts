0b9f9d14cd93292a9dd7175cdfc9b47fb1579a7b
Tobias Steinel <steinel@neundreiviertel.io>
2020-06-08 21:51:28 +0200

    ci: 🎡 prepared for usage as node module
    Took 6 minutes

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 /* Entities */
-import { AddressEntity } from './address/address.entity';
-import { CategoryEntity } from './category/category.entity';
-import { InstitutionEntity } from './institution/institution.entity';
-import { InstitutionAdminEntity } from './institutionadmin/institutionAdmin.entity';
-import { InstitutionalItemEntity } from './institutionalItem/institutionalItem.entity';
-import { InstitutionalOfferEntity } from './institutionalOffer/institutionalOffer.entity';
-import { InstitutionalRequestEntity } from './institutionalRequest/institutionalRequest.entity';
-import { InstitutionMemberEntity } from './institutionMember/institutionMember.entity';
-import { InstitutionTypeEntity } from './institutionType/institutionType.entity';
-import { PrivateItemEntity } from './privateItem/institutionalItem.entity';
-import { PrivateOfferEntity } from './privateOffer/privateOffer.entity';
-import { PrivateRequestEntity } from './privateRequest/privateRequest.entity';
-import { RewardCatalogueEntity } from './rewardCatalogue/rewardCatalogue.entity';
-import { TagEntity } from './tag/tag.entity';
-import { UserEntity } from './user/user.entity';
+import { AddressEntity } from './src/entities/address/address.entity';
+import { CategoryEntity } from './src/entities/category/category.entity';
+import { InstitutionEntity } from './src/entities/institution/institution.entity';
+import { InstitutionAdminEntity } from './src/entities/institutionadmin/institutionAdmin.entity';
+import { InstitutionalItemEntity } from './src/entities/institutionalItem/institutionalItem.entity';
+import { InstitutionalOfferEntity } from './src/entities/institutionalOffer/institutionalOffer.entity';
+import { InstitutionalRequestEntity } from './src/entities/institutionalRequest/institutionalRequest.entity';
+import { InstitutionMemberEntity } from './src/entities/institutionMember/institutionMember.entity';
+import { InstitutionTypeEntity } from './src/entities/institutionType/institutionType.entity';
+import { PrivateItemEntity } from './src/entities/privateItem/institutionalItem.entity';
+import { PrivateOfferEntity } from './src/entities/privateOffer/privateOffer.entity';
+import { PrivateRequestEntity } from './src/entities/privateRequest/privateRequest.entity';
+import { RewardCatalogueEntity } from './src/entities/rewardCatalogue/rewardCatalogue.entity';
+import { TagEntity } from './src/entities/tag/tag.entity';
+import { UserEntity } from './src/entities/user/user.entity';
 
-export = {
+module.exports = {
   AddressEntity,
   CategoryEntity,
   InstitutionEntity,
