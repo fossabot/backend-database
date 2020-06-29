34d689b01371d392233429f337b295e52286bb14
Bastian <49070966+BastianGaming@users.noreply.github.com>
2020-06-29 14:57:42 +0200

    Add InstitutionMember dto
    Add a basic InstitutionMember dto;
    may need some additions

---
+++ src/entities/institutionMember/requests.dto.ts
@@ -0,0 +1,47 @@
+import { IsDate, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Gender } from '../..';
+
+export class CreateInstitutionMemberDto {
+
+    @ApiProperty({ type: Gender })
+    @IsNotEmpty()
+    @IsEnum(Gender)
+    gender: Gender;
+
+    @ApiProperty({ type: "string" })
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @ApiProperty({ type: "string" })
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @ApiProperty({ type: "string", format: "email" })
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({ type: "date", required: false })
+    @IsOptional()
+    @IsDate()
+    dob?: Date;
+
+    @ApiProperty({ type: "string", required: false })
+    @IsOptional()
+    @IsEmail()
+    biography?: string;
+
+    @ApiProperty({ type: "boolean"})
+    @IsNotEmpty()
+    @IsBoolean()
+    verified: boolean; 
+    
+    @ApiProperty({ type:"string", required: false})
+    @IsOptional()
+    @IsString()
+    job_title?: string; 
+
+}
---
+++ src/entities/institutionMember/responses.dto.ts
@@ -0,0 +1,71 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUUID, IsEmail, IsNotEmpty, IsEnum, IsOptional, IsDate, IsBoolean } from "class-validator";
+import { Gender } from "../../enums/Gender.enum";
+
+export class GetInstitutionMemberDto {
+
+    @ApiProperty({type:"string", format: "uuid"})
+    @IsString()
+    @IsUUID()
+    id: string; 
+
+    @ApiProperty({ type:"string" })
+    @IsNotEmpty()
+    @IsString()
+    institution: string; 
+
+    @ApiProperty({ type:Gender })
+    @IsNotEmpty()
+    @IsEnum(Gender)
+    gender:Gender;
+
+    @ApiProperty({ type:"string"})
+    @IsNotEmpty()
+    @IsString()
+    name: string; 
+
+    @ApiProperty({ type:"string"})
+    @IsNotEmpty()
+    @IsString()
+    username: string; 
+
+    @ApiProperty({ type: "string", format: "email"})
+    @IsNotEmpty()
+    @IsEmail()
+    email: string; 
+
+    @ApiProperty({ type:"date", required: false})
+    @IsOptional()
+    @IsDate()
+    dob?: Date; 
+
+    @ApiProperty({ type:"string", required:false})
+    @IsOptional()
+    @IsString()
+    biography?: string; 
+
+    @ApiProperty({type:"boolean"})
+    @IsNotEmpty()
+    @IsBoolean()
+    verified: boolean; 
+
+    @ApiProperty({type:"string", required:false })
+    @IsOptional()
+    @IsString()
+    job_title?: string; 
+
+}
+
+export class GetInstitutionMemberPublicDto {
+
+    @ApiProperty({ type:"string"})
+    @IsNotEmpty()
+    @IsString()
+    name: string; 
+
+    @ApiProperty({ type: "string", format: "email"})
+    @IsNotEmpty()
+    @IsEmail()
+    email: string; 
+
+}
